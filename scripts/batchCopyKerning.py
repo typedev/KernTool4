ed43cd019b4e650f223203c7385d24c3e12cabd0
Alexander Lubovenko <lubovenko@me.com>
2022-08-23 01:38:24 +0300

    batch copy kern

--- scripts/batchCopyKerning.py
+++ scripts/batchCopyKerning.py
@@ -8,6 +8,30 @@ from pygments.lexer import RegexLexer, bygroups
 from pygments.token import *
 from pygments.styles import get_style_by_name, get_all_styles
 
+
+
+EDITMODE_KERNING = 1
+EDITMODE_MARGINS = 2
+EDITMODE_OFF = 0
+
+SIDE_1 = 'L' # left side
+SIDE_2 = 'R' # right side
+
+
+ID_KERNING_GROUP = 'public.kern'
+ID_GROUP_LEFT = '.kern1.'
+ID_GROUP_RIGHT = '.kern2.'
+
+ID_GROUP_MASK_1 = ID_KERNING_GROUP.replace('.kern', ID_GROUP_LEFT) #+ ID_GROUP_LEFT
+ID_GROUP_MASK_2 = ID_KERNING_GROUP.replace('.kern', ID_GROUP_RIGHT) #+ ID_GROUP_RIGHT
+
+ID_MARGINS_GROUP = 'public.margins' # '.margins'
+ID_MARGINS_GROUP_LEFT = '.margins1.'
+ID_MARGINS_GROUP_RIGHT  = '.margins2.'
+
+ID_GROUP_MARGINS_MASK_1 = ID_MARGINS_GROUP.replace('.margins', ID_MARGINS_GROUP_LEFT) #+ ID_MARGINS_GROUP_LEFT
+ID_GROUP_MARGINS_MASK_2 = ID_MARGINS_GROUP.replace('.margins', ID_MARGINS_GROUP_RIGHT) #+ ID_MARGINS_GROUP_RIGHT
+
 def getFontFilesList (fontsFolder):
 	filepath = os.path.join(fontsFolder, '*.ufo')
 
@@ -39,10 +63,10 @@ def diffOrderOnly (g1, g2):
 
 
 def saveKernPattern(patternlist, filename, pattern2line = 8):
-	report = []
-	report.append('* Saving Pattern to file:')
+	# report = []
+	# report.append('* Saving Pattern to file:')
 	fn = filename
-	report.append(fn)
+	# report.append(fn)
 	groupsfile = open(fn, mode = 'w')
 	txt = ''
 	p2l = 0
@@ -56,7 +80,7 @@ def saveKernPattern(patternlist, filename, pattern2line = 8):
 	groupsfile.write(txt)
 	groupsfile.close()
 	# report.append('= Done.')
-	return report
+	# return report
 
 def makePatternsFromPairsList(host, font, pairslist):
 	patterns = []
@@ -68,7 +92,7 @@ def makePatternsFromPairsList(host, font, pairslist):
 		pairs.append((lg,rg))
 	for pair in sorted(pairs):
 		l, r = pair
-		pattern = host.langSet.wrapPairToPattern(font, (l, r))
+		pattern = ('space', l, r, 'space')
 		patterns.append(pattern)
 	return patterns
 
@@ -81,12 +105,12 @@ def filterKernGroups(font):
 
 
 def diffGlyphsTable(sourcefont, newfont):
-	result = []
+	glyphslist = []
 
 	for glyphname in newfont.glyphOrder:
 		if glyphname not in sourcefont:
-			result.append(glyphname)
-
+			glyphslist.append(glyphname)
+	return glyphslist
 
 
 def diffGroups (masterfont, targetfont, ignoreOrder = True):
@@ -158,7 +182,7 @@ class BaseLexerCKBL(RegexLexer):
 	        # (r'(after:.*)(".*")(.*)\n', bygroups(String, Text, String)),
 	        # (r'(content:)(.*)(".*")(.*)\n', bygroups(Keyword, String, Text, String)),
 	        (r'not found:.*\n', Error),
-	        (r'(content:)(.*)\n', bygroups(Keyword, Text)),
+	        (r'(new glyphs:)(.*)\n', bygroups(Keyword, Text)),
 	        (r'\+.*\n', Generic.Emph),
 	        (r'-.*\n', Error),
             (r'@.*\n', Generic.Subheading),
@@ -168,6 +192,137 @@ class BaseLexerCKBL(RegexLexer):
             (r'.*\n', Text),
         ],
     }
+
+
+class TDHashGroupsDic(object):
+	def __init__ (self, font, langSet = None):
+		self.leftDic = {}
+		self.rightDic = {}
+		self.leftMarginsDic = {}
+		self.rightMarginsDic = {}
+		self.dicOfKeyGlyphsByGroup = {}
+		self.font = font
+		self.langSet = langSet
+		self.history = []
+		self.trackHistory = True
+		self.makeReverseGroupsMapping()
+		# self.langSet = TDLangSet()
+
+	def setFont(self, font, langSet = None):
+		# print ('setup hashkernDic')
+		self.leftDic = {}
+		self.rightDic = {}
+		self.leftMarginsDic = {}
+		self.rightMarginsDic = {}
+		self.dicOfKeyGlyphsByGroup = {}
+		self.font = font
+		self.langSet = langSet
+		self.history = []
+		self.makeReverseGroupsMapping()
+
+	def clearHistory(self):
+		self.history = []
+
+	def setHistoryPause(self):
+		self.trackHistory = False
+	def setHistoryResume(self):
+		self.trackHistory = True
+
+
+	def isLeftSideGroup (self, groupname):
+		result = False
+		# if groupname[ID_GROUP_DIRECTION_POSITION] == SIDE_1:
+		if ID_GROUP_LEFT in groupname or ID_MARGINS_GROUP_LEFT in groupname:
+			result = True
+		return result
+
+	def isKerningGroup (self, groupname):
+		if groupname.startswith(ID_KERNING_GROUP):
+			return True
+		return False
+
+	def isMarginsGroup(self, groupname):
+		if groupname.startswith(ID_MARGINS_GROUP):
+			return True
+		return False
+
+	def checkMapAndAddGlyph2hashMap(self, dic, glyphname, groupname):
+		if glyphname not in dic:
+			dic[glyphname] = groupname
+			return True
+		else:
+			print ('ERROR: %s already in group %s and %s' % (glyphname, dic[glyphname], groupname))
+			print ('The extension may not work correctly.\nPlease decide in which group to leave this glyph and restart the extension')
+			return False
+
+	def makeReverseGroupsMapping (self):
+		self.leftDic = {}
+		self.rightDic = {}
+		self.leftMarginsDic = {}
+		self.rightMarginsDic = {}
+		self.dicOfKeyGlyphsByGroup = {}
+		for groupname, content in self.font.groups.items():
+			if content:
+				self.dicOfKeyGlyphsByGroup[groupname] = content[0]
+			if self.isKerningGroup(groupname):
+				if self.isLeftSideGroup(groupname):
+					for glyphname in content:
+						# self.leftDic[glyphname] = groupname
+						self.checkMapAndAddGlyph2hashMap(self.leftDic, glyphname, groupname)
+				else:
+					for glyphname in content:
+						# self.rightDic[glyphname] = groupname
+						self.checkMapAndAddGlyph2hashMap(self.rightDic, glyphname, groupname)
+			elif self.isMarginsGroup(groupname):
+				if self.isLeftSideGroup(groupname):
+					for glyphname in content:
+						# self.leftMarginsDic[glyphname] = groupname
+						self.checkMapAndAddGlyph2hashMap(self.leftMarginsDic, glyphname, groupname)
+
+				else:
+					for glyphname in content:
+						# self.rightMarginsDic[glyphname] = groupname
+						self.checkMapAndAddGlyph2hashMap(self.rightMarginsDic, glyphname, groupname)
+
+
+	def getGroupNameByGlyph (self, glyphname, side, mode = EDITMODE_KERNING):
+		if mode == EDITMODE_KERNING:
+			if side == SIDE_1 and glyphname in self.leftDic:
+				return self.leftDic[glyphname]
+			if side == SIDE_2 and glyphname in self.rightDic:
+				return self.rightDic[glyphname]
+		elif mode == EDITMODE_MARGINS:
+			if side == SIDE_1 and glyphname in self.leftMarginsDic:
+				return self.leftMarginsDic[glyphname]
+			if side == SIDE_2 and glyphname in self.rightMarginsDic:
+				return self.rightMarginsDic[glyphname]
+		return glyphname
+
+	def thisGlyphInGroup(self, glyphname, side, mode = EDITMODE_KERNING):
+		if mode == EDITMODE_KERNING:
+			if side == SIDE_1 and glyphname in self.leftDic:
+				return True
+			if side == SIDE_2 and glyphname in self.rightDic:
+				return True
+		elif mode == EDITMODE_MARGINS:
+			if side == SIDE_1 and glyphname in self.leftMarginsDic:
+				return True
+			if side == SIDE_2 and glyphname in self.rightMarginsDic:
+				return True
+		return False
+
+	def getKeyGlyphByGroupname(self, groupname):
+		if groupname in self.dicOfKeyGlyphsByGroup:
+			return self.dicOfKeyGlyphsByGroup[groupname]
+		else:
+			return groupname
+
+	def getPairsBy(self, key, side):
+		if side == SIDE_1:
+			return list(filter(lambda p: p[0][0] == (key), self.font.kerning.items()))
+		elif side == SIDE_2:
+			return list(filter(lambda p: p[0][1] == (key), self.font.kerning.items()))
+
 
 class TDCopyKernWindow(object):
 	def __init__ (self, ):
@@ -188,7 +343,7 @@ class TDCopyKernWindow(object):
 		                                                    ],
 		                                )
 		self.w.bar = vanilla.ProgressBar('auto')
-		self.w.btnRun = vanilla.Button('auto', title = 'Run', callback = self.btnRunCallback)  # ,
+		self.w.btnRun = vanilla.Button('auto', title = 'Start Copy kerning', callback = self.btnRunCallback)  # ,
 		self.w.textBox = CodeEditor('auto', text = '', readOnly = True, showLineNumbers = False, checksSpelling = False)
 		self.w.textBox.setLexer(BaseLexerCKBL())
 		self.w.textBox.setHighlightStyle(get_style_by_name('material'))
@@ -205,7 +360,7 @@ class TDCopyKernWindow(object):
 			"H:|-border-[btnRun]-border-|",
 			"H:|-0-[textBox]-0-|",
 			# Vertical
-			"V:|-border-[label1]-space-[fontA]-border-[label2]-border-[fontB]-border-[fontFolder]-border-[fontsList(==200)]-border-[bar]-border-[btnRun]-[textBox]-0-|",
+			"V:|-border-[label1]-space-[fontA]-border-[label2]-space-[fontB]-border-[fontFolder]-space-[fontsList(==150)]-space-[bar]-space-[btnRun]-[textBox]-0-|",
 		]
 		metrics = {
 			"border": 15,
@@ -218,22 +373,64 @@ class TDCopyKernWindow(object):
 	def btnRunCallback (self, sender):
 		fontA = self.fonts[self.w.fontA.get()]
 		fontB = self.fonts[self.w.fontB.get()]
+		hashKernDic = TDHashGroupsDic(fontB)
 		globalreport = []
+		glyphslist = diffGlyphsTable(fontA, fontB)
+		# # for glyphname in glyphslist:
+		globalreport.append('new glyphs: %s' % ' '.join(glyphslist))
+		self.w.bar.set(0)
+		inc = int(round(100 / len(self.targetFontList), 0))
 		for targetfont in self.targetFontList:
+
 			report = diffGroups(fontB, targetfont, ignoreOrder = True)
 			globalreport.extend(report)
-		globalreport.append('\n')
-		self.w.textBox.set('\n'.join(globalreport))
+			pairs2copy = {}
+			for glyphname in glyphslist:
+				lpairs = hashKernDic.getPairsBy(glyphname, SIDE_1)
+				rpairs = hashKernDic.getPairsBy(glyphname, SIDE_2)
+				lg = hashKernDic.getGroupNameByGlyph(glyphname, SIDE_1)
+				rg = hashKernDic.getGroupNameByGlyph(glyphname, SIDE_2)
+				if lg != glyphname:
+					lpairs.extend(hashKernDic.getPairsBy(lg,SIDE_1))
+				if rg != glyphname:
+					rpairs.extend(hashKernDic.getPairsBy(rg,SIDE_2))
+				# globalreport.append('%s' % glyphname)
+				# globalreport.append('\tside1:')
+				for (l, r), v in lpairs:
+					if (l,r) not in targetfont.kerning and (l,r) not in fontA.kerning:
+						pairs2copy[(l,r)] = v
+						targetfont.kerning[(l,r)] = v
+				for (l, r), v in rpairs:
+					if (l, r) not in targetfont.kerning and (l, r) not in fontA.kerning:
+						pairs2copy[(l, r)] = v
+						targetfont.kerning[(l, r)] = v
 
-		# checkPatternsLib(host = self.parent.hashKernDic, fonts = [fontA, fontB])
-		# report = transferKern(host = self.parent.hashKernDic, masterfont = fontA, targetfont = fontB, applyTransfer = self.w.applyTransfer.get())
-		# self.w.textBox.set('\n'.join(report))
+
+			globalreport.append('= total copied pairs: %i' % len(pairs2copy))
+			workPath = os.path.join(targetfont.path.replace(os.path.basename(targetfont.path), ''))
+
+			fpattern = os.path.join(workPath, 'Patterns to check - %s.txt' % (os.path.basename(targetfont.path.replace('.ufo',''))))
+			globalreport.append('* Patterns saved to file: \n%s' % fpattern)
+			patterns = makePatternsFromPairsList(hashKernDic, targetfont, pairs2copy)
+			saveKernPattern(patterns, fpattern)
+			targetfont.save()
+			self.w.bar.set(inc)
+			inc += inc
+		globalreport.append('\n')
+
+		self.w.textBox.set('\n'.join(globalreport))
+		for targetfont in self.targetFontList:
+			targetfont.close()
+
+
 
 	def fontFolderCallback (self, sender):
 		result = getFolder('Choose Target Folder with UFO files')
 
 		if result:
 			self.w.fontsList.set([])
+			fontA = self.fonts[self.w.fontA.get()]
+			fontB = self.fonts[self.w.fontB.get()]
 			items = []
 			txt = ''
 			paths = getFontFilesList(result[0])
@@ -241,26 +438,27 @@ class TDCopyKernWindow(object):
 			self.w.bar.set(0)
 			for fontpath in paths:
 				font = OpenFont(fontpath, showInterface = False)
-				self.targetFontList.append(font)
-				ufo = os.path.basename(fontpath)
-				family = font.info.familyName
-				style = font.info.styleName
-				item = dict(
-					UFO = ufo,
-					Family = family,
-					Style = style
-				)
-				items.append(item)
-				self.w.bar.increment(inc)
-				# inc += inc
-
+				if font != fontA and font != fontB:
+					self.targetFontList.append(font)
+					ufo = os.path.basename(fontpath)
+					family = font.info.familyName
+					style = font.info.styleName
+					item = dict(
+						UFO = ufo,
+						Family = family,
+						Style = style
+					)
+					items.append(item)
+				self.w.bar.set(inc)
+				inc += inc
+			self.w.bar.set(100)
 			self.w.fontsList.set(items)
 			# print (getFontFilesList(result[0]))
-		report = []
-		for f in self.targetFontList:
-			report.append('+ %s %s' % (f.info.familyName, f.info.styleName))
-		report.append('\n')
-		self.w.textBox.set('\n'.join(report))
+		# report = []
+		# for f in self.targetFontList:
+		# 	report.append('+ %s %s' % (f.info.familyName, f.info.styleName))
+		# report.append('\n')
+		# self.w.textBox.set('\n'.join(report))
 
 
 #
